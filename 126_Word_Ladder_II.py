dfa71707bdd2d5a6803f8e579756aae4706f12c6
BenMosbah <benmosbahmohamed@gmail.com>
2023-03-03 17:09:58 +0000

    More work on 126

--- 126_Word_Ladder_II.py
+++ 126_Word_Ladder_II.py
@@ -35,4 +35,33 @@ class Solution:
         if len(sequences_found) > 0:
             shortest = min([len(s) for s in sequences_found])
         shortest_sequences = [s for s in sequences_found if len(s) == shortest]
-        return [[beginWord] + s for s in shortest_sequences]
+
+        #return [[beginWord] + s for s in shortest_sequences]
+
+        ##### DFS solution
+        if endWord not in wordList :
+            return []
+        else:
+            found = False
+            chain_candidates = [[endWord]]
+            depth = 1
+            final_chains = []
+            while not found and depth < len(wordList):
+                to_build_new_chain_candidates = []
+                for chain in chain_candidates:
+                    available_words = wordSet-set(chain)
+                    new_endword = chain[0]
+                    new_chains = []
+                    if dist_is_one(beginWord,new_endword):
+                        final_chains.append([beginWord]+chain)
+                        found = True
+                    else:
+                        neighbors = [w for w in available_words if dist_is_one(w,new_endword)]
+                        if len(neighbors) == 0 :
+                            pass
+                        else:
+                            new_chains = [[neighbor]+chain for neighbor in neighbors]
+                    to_build_new_chain_candidates += new_chains
+                depth += 1
+                chain_candidates = to_build_new_chain_candidates
+            return final_chains
